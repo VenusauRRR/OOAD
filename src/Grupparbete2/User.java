db50b03847264fd8159609bd550d4dc3af4aef86
Venus Poon <venuspsw@hotmail.com>
2023-12-14 00:29:13 +0100

    edit lines

--- src/Grupparbete2/User.java
+++ src/Grupparbete2/User.java
@@ -1,13 +1,8 @@
 package Grupparbete2;
-
-import java.util.Scanner;
-
 public class User {
     public static void main(String[] args) {
-        int userChoice;
         Content c = new Content();
         c.setPage(new Loginpage(c));
         c.executepage();
-
     }
 }
